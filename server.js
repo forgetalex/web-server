0866a19462e85be7c903d90a8e1c8a78ddb5a2c2
Alexei Scerbakov <scerbakov.alexei@gmail.com>
2016-02-26 15:14:13 +0100

    Update port for heroku

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var middleware = require('./middleware.js')
-
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log(req.method);
-		next();
-	}
-};
 
 //for every route
 app.use(middleware.logger);
